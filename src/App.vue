3b5a40c90bb58bd0d0648bc1eb58decab17ae0b3
Maxime Dardenne <dardennemaxime.pro@gmail.com>
2025-03-18 08:47:19 +0100

    ajout bouton de menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
     import ButtonIdenticator from './components/buttons/ButtonIdenticator.vue';
+    import ButtonMenu from './components/buttons/ButtonMenu.vue';
 
 </script>
 
 <template>
 	  <ButtonIdenticator label="Primary" classArray="primary"></ButtonIdenticator>
+    <ButtonMenu classArray="normalSize" icon="door-open"></ButtonMenu>
 </template>
---
+++ src/components/buttons/ButtonMenu.vue
@@ -0,0 +1,41 @@
+<template>
+    <button>
+        <font-awesome-icon :class="classArray" class="icon, button" :icon="['fas', icon ]" /> <!-- icon="play, pause, rotate-right, door-open exit" -->
+    </button>  
+</template>
+
+<script setup>
+    const props = defineProps({
+        icon : {
+            type: String,
+            required: true,
+        },
+        classArray: Array
+    });
+</script>
+
+<style scoped>
+    button {
+        border-radius: 50px;
+        border: none;
+        transition: all ease 0.2s;
+    }
+
+    .icon {
+        color: black;
+        font-size: 30px;
+    }
+
+    .normalSize {
+        font-size: 20px;
+        width: 60px;
+        height: 60px;
+    }
+
+    .littleSize {
+        font-size: 10px;
+        width: 40px;
+        height: 40px;
+    }
+    
+</style>
