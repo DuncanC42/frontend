58b612451097674d0fd2acaf19ad871fe14d2930
Nathan <n.filaux@gmail.com>
2025-03-18 10:09:49 +0100

    ✨ volume en localstorage

--- src/stores/volume.js
+++ src/stores/volume.js
@@ -2,15 +2,17 @@ import { defineStore } from 'pinia';
 
 export const volumeStore = defineStore('global', {
     state: () => ({
-        musique: Math.pow(50 / 100, 2),
-        effet_sonore: Math.pow(50 / 100, 2),
+        musique: JSON.parse(localStorage.getItem('musique')) ?? Math.pow(50 / 100, 2),
+        effet_sonore: JSON.parse(localStorage.getItem('effet_sonore')) ?? Math.pow(50 / 100, 2),
     }),
     actions: {
         setMusique(newVolume) {
             this.musique = Math.pow(newVolume / 100, 2);
+            localStorage.setItem('musique', JSON.stringify(this.musique));
         },
         setEffetSonore(newVolume) {
             this.effet_sonore = Math.pow(newVolume / 100, 2);
+            localStorage.setItem('effet_sonore', JSON.stringify(this.effet_sonore));
         },
     },
 });
