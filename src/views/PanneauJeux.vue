4dfaf31c2ca3056b36b566d76101bd8742bc556b
Nathan <n.filaux@gmail.com>
2025-03-19 09:00:12 +0100

    🚧 début page panneau

---
+++ src/views/PanneauJeux.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Image from '../assets/images/PanneauLiege.png'
+import FondEcran from '@/components/FondEcran.vue';
+
+
+
+</script>
+
+<template>
+    <FondEcran :image="Image"></FondEcran>
+
+</template>
