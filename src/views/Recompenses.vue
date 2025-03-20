c35025fc769dd2c64a6de97bfc36b8ef7be453b2
Nathan <n.filaux@gmail.com>
2025-03-20 15:07:37 +0100

    ✨ création de la page des cadeaux

---
+++ src/views/Recompenses.vue
@@ -0,0 +1,66 @@
+<script setup>
+import gift_1 from '@/assets/images/gift_1.png'
+import gift_2 from '@/assets/images/gift_2.png'
+import Appartement from '@/assets/images/Appartement.png'
+import FondEcran from '@/components/FondEcran.vue';
+import BlurFilter from '@/components/BlurFilter.vue';
+import CompteRebours from '@/components/temps/CompteRebours.vue';
+</script>
+
+<template>
+    <div class="main">
+        <BlurFilter :is-open="true"></BlurFilter>
+        <FondEcran :image="Appartement"></FondEcran>
+        <div class="countdown">
+            <h1>Distribution des récompenses dans</h1>
+            <CompteRebours :target-date="'04/01/2025'"></CompteRebours>
+        </div>
+        <div class="gifts">
+            <img :src="gift_1" alt="Image cadeau n°1">
+            <img :src="gift_2" alt="Image cadeau n°2">
+        </div>
+        
+    </div>
+
+</template>
+
+<style scoped>
+.main{
+    display: flex;
+    justify-content: space-evenly;
+    color:white;
+    text-align: center;
+}
+
+.main *{
+    z-index: 100;
+}
+
+.countdown{
+    padding: 0 10px;
+}
+
+.countdown h1{
+    text-decoration: underline;
+    font-weight: 500;
+}
+
+.gifts {
+    display: flex;
+    justify-content: space-between;
+    padding: 2vw;
+    height:40vw;
+}
+
+img {
+    width: 45vw;
+}
+
+img:first-of-type{
+    align-self: flex-start;
+}
+
+img:last-of-type{
+    align-self: flex-end;
+}
+</style>
