2e8362011d9c0c57d798fc1b8e89afc9284b3b7e
Nathan <134612840+nfilaux@users.noreply.github.com>
2025-03-21 17:36:11 +0100

    SCRUM-22/jeu tirelire (#19)
    * 🍱 ajout des assets pour tirelire
    
    * 🚧 drag tirelire fonctionne !
    
    * 🚧 bon picto ajoute des points et mauvais augmentent le temps
    
    * 🎨 ajout des sprites de jauge et mise à jour de la logique de score
    
    * 🎶 ajout de la gestion audio avec des effets sonores et ambiance
    
    * 🔨 modification des textes & ajout gestion volumes effets sonores
    
    * 🎨 made a file for assets loading
    
    * ✨ ajout du chrono

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
 import NavBar from './components/NavBar.vue';
 import Header from './components/Header.vue'
 import VolumeButton from './components/volumes/VolumeButton.vue'
-import { useMusique } from './composable/volumes'
-useMusique();
+import { useMusic } from '@/composable/volumes';
+import audio_mp3 from '@/assets/sons/musiques/ambiance/chill.mp3';
+
+useMusic(audio_mp3);
 
 
 </script>
 
 <template>
   <div>
-    <NavBar /> <!-- Barre de navigation -->
+    <!-- <NavBar /> -->
     <Header></Header>
     <RouterView></RouterView>
     <VolumeButton></VolumeButton>
@@ -20,11 +22,10 @@ useMusique();
 
 
 <style scoped>
-div{
-	display: flex;
-	flex-direction: column;
-	height: 100vh;
-	width: 100vw;
+div {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
 }
-
 </style>
