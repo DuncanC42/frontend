846d0fb3586024b4d9d82d71fda4e692c4d05984
Nathan <n.filaux@gmail.com>
2025-03-31 17:29:49 +0200

    sticky score

---
+++ src/components/TableauLeaderboard.vue
@@ -0,0 +1,154 @@
+<script setup>
+
+import { ref } from 'vue'
+import { fetchBackend } from '@/composable/fetchBackend';
+import { useInfiniteScroll } from '@vueuse/core'
+
+const list = ref(null)
+
+let counter = 1
+
+
+const props = defineProps({
+    route: {
+        type: String,
+        required: true,
+    },
+    myPlace: {
+        type: Number,
+        required: true,
+    }
+})
+
+useInfiniteScroll(list, () => {
+    classement.value.push({
+        place: counter,
+        pseudo: 'Player ' + counter,
+        points: Math.round(2000 / counter),
+    })
+    counter++
+},
+    {
+        distance: 1,
+        canLoadMore: () => {
+            return true
+        },
+    }
+)
+
+const classement = ref([])
+
+
+</script>
+
+<template>
+    <div class="leaderboard" ref="list">
+        <div v-for="(score, index) in classement" :key="index" class="row-container">
+
+            <hr v-if="score.place - 1 == myPlace" />
+            <div class="row"
+                :class="{ 'gold-text': score.place === 1, 'silver-text': score.place === 2, 'copper-text': score.place === 3, 'sticky': myPlace == score.place }">
+                <span>{{ score.place }}</span>
+                <span>{{ score.pseudo }}</span>
+                <span>{{ score.points }} pts</span>
+            </div>
+            <!-- Only add hr if not the last item -->
+            <hr v-if="index !== classement.length - 1 && score.place != myPlace" />
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped>
+.leaderboard {
+    background-color: #ffffff78;
+    height: 60vh;
+    width: 85vw;
+    border-radius: 20px;
+    overflow-y: auto;
+    color: black;
+    font-size: x-large;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 15px 0;
+    z-index: 102;
+    /* Added for sticky to work correctly */
+    position: relative;
+    /* Add padding at the top of the scroll area */
+}
+
+.row-container {
+    width: 85%;
+    position: relative;
+    /* Important for sticky positioning context */
+}
+
+.row {
+    display: grid;
+    grid-template-columns: 2fr 4fr 3fr;
+    width: 100%;
+    padding: 5px 0;
+}
+
+.rank {
+    justify-self: start;
+}
+
+.player {
+    justify-self: center;
+}
+
+.points {
+    justify-self: end;
+}
+
+hr {
+    height: 1px;
+    background-color: rgba(0, 0, 0, 0.523);
+    border: none;
+    margin: 0;
+    margin: 10px 0;
+}
+
+.gold-text {
+    color: rgb(255, 217, 0);
+}
+
+.silver-text {
+    color: #ffffff;
+}
+
+.copper-text {
+    color: #ffa938;
+}
+
+.copper-text,
+.silver-text,
+.gold-text {
+    font-weight: bold;
+}
+
+.sticky {
+    position: sticky;
+    top: 0;
+    position: relative;
+
+}
+
+/* Make the container of the sticky row also sticky */
+.row-container:has(.sticky) {
+    position: sticky;
+    top: 0;
+    bottom: 0;
+    z-index: 5;
+    margin: 0;
+    padding: 0;
+}
+
+/* Fix for the last item's hr condition */
+.row-container:last-child hr {
+    display: none;
+}
+</style>
--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -5,9 +5,8 @@ import FondEcran from '@/components/FondEcran.vue';
 import appartement from '@/assets/images/Appartement.png';
 import BlurFilter from '@/components/BlurFilter.vue';
 import ButtonNextPrevious from '@/components/buttons/ButtonNextPrevious.vue';
-import { fetchBackend } from '@/composable/fetchBackend';
-import { useInfiniteScroll } from '@vueuse/core'
-import { ref, useTemplateRef } from 'vue';
+import TableauLeaderboard from '@/components/TableauLeaderboard.vue';
+
 
 const props = defineProps({
     route: {
@@ -24,20 +23,6 @@ const props = defineProps({
     }
 })
 
-const list = ref(null)
-
-useInfiniteScroll(list, () => {
-    console.log('fetch new data')
-},
-    {
-        distance: 10,
-        canLoadMore: () => {
-            return true
-        },
-    }
-)
-
-
 </script>
 
 <template>
@@ -47,17 +32,7 @@ useInfiniteScroll(list, () => {
                 <img :src="podium" :alt="podium">
                 <h2>{{ title }}</h2>
             </div>
-            <div class="leaderboard" ref="list">
-                <div v-for="(r, index) in 100" :key="index">
-                    <div class="row" :class="{ 'gold-text': r === 1, 'silver-text': r === 2, 'copper-text': r === 3 }">
-                        <span>{{ r }}</span>
-                        <span>Joueur {{ r }}</span>
-                        <span>{{ (100 - Number(r)) * 100 }} pts</span>
-                    </div>
-                    <!-- Only add hr if not the last item -->
-                    <hr v-if="index < 99">
-                </div>
-            </div>
+            <TableauLeaderboard route="general" my-place="26"></TableauLeaderboard>
             <div class="pagination">
                 <ButtonNextPrevious :class="page === 1 ? 'hidden' : ''" :classArray="['previous']" />
                 <div class="dot" v-for="i in 6" :id="i + 1" :class="i === page ? 'lighter-dot' : ''"></div>
@@ -97,41 +72,6 @@ useInfiniteScroll(list, () => {
     gap: 20px;
 }
 
-.leaderboard {
-    background-color: #ffffff78;
-    height: 60vh;
-    width: 85vw;
-    border-radius: 20px;
-    overflow-y: auto;
-    color: black;
-    font-size: x-large;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 15px 0;
-    z-index: 102;
-}
-
-.leaderboard>div {
-    width: 85%;
-    /* Add some margin between items */
-}
-
-.row {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    /* Remove the global outline */
-}
-
-hr {
-    height: 1px;
-    background-color: rgba(0, 0, 0, 0.523);
-    border: none;
-    margin: 0;
-    margin: 10px 0;
-}
-
 .pagination {
     display: flex;
     justify-content: space-evenly;
@@ -152,24 +92,5 @@ hr {
 
 .hidden {
     visibility: hidden;
-}
-
-/* Medal colors for top three ranks */
-.gold-text {
-    color: rgb(255, 217, 0);
-}
-
-.silver-text {
-    color: #ffffff;
-}
-
-.copper-text {
-    color: #ffa938;
-}
-
-.copper-text,
-.silver-text,
-.gold-text {
-    font-weight: bold;
 }
 </style>
